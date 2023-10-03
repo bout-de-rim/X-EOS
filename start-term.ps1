4f6f20d2dafc3d7a8be45a212ebfbbfbd826c81d
bout-de-rim <bout-de-rim@users.noreply.github.com>
2023-10-04 01:03:07 +0200

    Script to settup env in VS Code

---
+++ start-term.ps1
@@ -0,0 +1,3 @@
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process 
+& "c:/Users/Rémi/Documents/11 - Dev/X-EOS/.venv/Scripts/Activate.ps1"
+$env:PYTHONPATH += ";C:\Users\Rémi\Documents\11 - Dev\X-EOS\src"
