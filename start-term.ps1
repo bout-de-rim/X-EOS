9cd70c0c375fe5f2107df6c4306ad7b074ca0b41
bout-de-rim <bout-de-rim@users.noreply.github.com>
2024-03-15 12:12:57 +0100

    Remove start-term.ps1 script

--- start-term.ps1
+++
@@ -1,3 +0,0 @@
-Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope Process 
-& "c:/Users/Rémi/Documents/11 - Dev/X-EOS/.venv/Scripts/Activate.ps1"
-$env:PYTHONPATH += ";C:\Users\Rémi\Documents\11 - Dev\X-EOS\src"
