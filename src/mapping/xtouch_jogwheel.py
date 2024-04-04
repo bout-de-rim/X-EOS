4cc063ec3ba2d4e2eb5f900649474c53764b83da
bout-de-rim <bout-de-rim@users.noreply.github.com>
2024-04-04 11:56:57 +0200

    imple jogwheel

--- src/mapping/xtouch_jogwheel.py
+++ src/mapping/xtouch_jogwheel.py
@@ -1,15 +1,23 @@
 import time
+import statistics
 
 class JogWheelHandler:
     def __init__(self):
         self.last_time = time.perf_counter()
+        self.time_diffs = []
 
     def handle(self, value):
         current_time = time.perf_counter()
         time_diff = current_time - self.last_time
         self.last_time = current_time
 
-        multiplier = self.calculate_multiplier(time_diff)
+        self.time_diffs.append(time_diff)
+        if len(self.time_diffs) > 4:  # Keep only the last 4 time_diffs
+            self.time_diffs.pop(0)
+
+        median_time_diff = statistics.median(self.time_diffs)
+
+        multiplier = self.calculate_multiplier(median_time_diff)
 
         value = int(value, 16)
         jog_value = (value & 0x3F) * (-1 if (value & 0x40) else 1)
